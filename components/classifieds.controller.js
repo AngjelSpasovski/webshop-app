afa148169768b6fd63d3913279e0790a31305206
Angjel Spasovski <angjel.spasovski@armedia.com>
2016-10-04 17:44:01 +0200

    HTTP promise Request

--- components/classifieds.controller.js
+++ components/classifieds.controller.js
@@ -1,141 +1,21 @@
 angular.module("ngClassifieds")
 
-.controller("classifiedsCtrl", function($scope){
+.controller("classifiedsCtrl", function($scope, $http){
 	
-	$scope.classifieds = [
+	/*
+		POST	- sending data
+		GET		- retrive data
+		PUT		- editing data
+		DELETE	- deleting datas
+	*/
+	$http.get("data/classifieds.json").then(function(classifieds){
+		
+		console.log(classifieds);
+		
+		$scope.classifieds = classifieds.data;
 
-		{
-			"id":"1",
-			"title":"Some title 1",
-			"description":"Some description longer than title",
-			"price":6000,
-			"posted":"26.09.2016",
-			"contact": {
-				"name":"Angjel Spasovski",
-				"phone":"(078) 384-653",
-				"email":"angjel.spasovski@gmail.com"
-			},
-			"categories":[
-			"Vehicles",
-			"Parts and Accessories"
-			],
-			"image":"images/dell.jpg",
-			"view":213
-			
-		},
-		{
-			"id":"2",
-			"title":"Some title 2",
-			"description":"Some description longer than title",
-			"price":6000,
-			"posted":"26.09.2016",
-			"contact": {
-				"name":"Pero Burgija",
-				"phone":"(078) 334-653",
-				"email":"pero.burgija@gmail.com"
-			},
-			"categories":[
-			"Vehicles",
-			"Parts and Accessories"
-			],
-			"image":"images/dell.jpg",
-			"view":276
-			
-		},
-		{
-			"id":"3",
-			"title":"Some title 3",
-			"description":"Some description longer than title",
-			"price":6000,
-			"posted":"26.09.2016",
-			"contact": {
-				"name":"Ibe Palikukja",
-				"phone":"(078) 312-653",
-				"email":"ibe.palikukja@gmail.com"
-			},
-			"categories":[
-			"Vehicles",
-			"Parts and Accessories"
-			],
-			"image":"images/ibe.jpg",
-			"view":2234
-			
-		},
-		{
-			"id":"4",
-			"title":"Some title",
-			"description":"Some description longer than title",
-			"price":6000,
-			"posted":"26.09.2016",
-			"contact": {
-				"name":"Angjel Spasovski",
-				"phone":"(078) 384-653",
-				"email":"angjel.spasovski@gmail.com"
-			},
-			"categories":[
-			"Vehicles",
-			"Parts and Accessories"
-			],
-			"image":"images/hp.jpg",
-			"view":213
-			
-		},
-		{
-			"id":"5",
-			"title":"Some title",
-			"description":"Some description longer than title",
-			"price":6000,
-			"posted":"26.09.2016",
-			"contact": {
-				"name":"Angjel Spasovski",
-				"phone":"(078) 384-653",
-				"email":"angjel.spasovski@gmail.com"
-			},
-			"categories":[
-			"Vehicles",
-			"Parts and Accessories"
-			],
-			"image":"images/lenovo.jpg",
-			"view":213
-			
-		},
-		{
-			"id":"6",
-			"title":"Some title",
-			"description":"Some description longer than title",
-			"price":6000,
-			"posted":"26.09.2016",
-			"contact": {
-				"name":"Angjel Spasovski",
-				"phone":"(078) 384-653",
-				"email":"angjel.spasovski@gmail.com"
-			},
-			"categories":[
-			"Vehicles",
-			"Parts and Accessories"
-			],
-			"image":"images/dell.jpg",
-			"view":213
-			
-		},
-		{
-			"id":"7",
-			"title":"Some title",
-			"description":"Some description longer than title",
-			"price":6000,
-			"posted":"26.09.2016",
-			"contact": {
-				"name":"Angjel Spasovski",
-				"phone":"(078) 384-653",
-				"email":"angjel.spasovski@gmail.com"
-			},
-			"categories":[
-			"Vehicles",
-			"Parts and Accessories"
-			],
-			"image":"images/ibe.jpg",
-			"view":213
-			
-		}
-	]
+		console.log(classifieds.data[0]);
+	});
+
+	
 });
