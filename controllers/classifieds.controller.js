b31ee7ead8dcc88409cca4fd10521c66c3344f75
Angjel Spasovski <angjel.spasovski@armedia.com>
2016-10-06 20:59:26 +0200

    Adding New data and  success message

--- controllers/classifieds.controller.js
+++ controllers/classifieds.controller.js
@@ -1,25 +1,44 @@
 angular.module("ngClassifieds")
 
-.controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav){
+.controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast){
 	
-	/*
-		POST	- sending data
-		GET		- retrive data
-		PUT		- editing data
-		DELETE	- deleting datas
-	*/
+	// call the service GET function
 	classifiedsFactory.getClassifieds().then(function(classifieds){
 		$scope.classifieds = classifieds.data;
 	});
 
-	// open side bar function 
-	$scope.openSidebar = function(){
-		$mdSidenav('left').open();
+	var contact = {									// static contact object data that faking user profile info
+		name:" Angjel Spasovski",
+		phone:"222-333-555",
+		email:"angjel.spasovski@yahoo.com"
 	}
 
 	// open side bar function 
+	$scope.openSidebar = function(){
+		$mdSidenav('left').open();					// open the slide nav
+	}
+
+	// close side bar function 
 	$scope.closeSidebar = function(){
-		$mdSidenav('left').close();
+		$mdSidenav('left').close();					// close the slide nav
+		$scope.classified = {};						// empty the fields
+	}
+
+	// save side bar function
+	$scope.saveClassified = function(classified){
+		if(classified){								// if is true (have data in object)
+			classified.contact = contact;			// send values from static object
+			$scope.classifieds.push(classified);	// push the data
+			$scope.classified = {};					// empty the fields 
+			$scope.closeSidebar();					// close the slide nav function
+			// message
+			$mdToast.show(
+				$mdToast.simple()
+						.content("Classified saved!")
+						.position('top, right')
+						.hideDelay(3000)
+			);
+		}
 	}
  
 
--- services/classifieds.service.js
+++ services/classifieds.service.js
@@ -2,6 +2,13 @@ angular.module("ngClassifieds")
 
 .factory("classifiedsFactory", function($http){
 	
+	/*
+		POST	- sending data
+		GET		- retrive data
+		PUT		- editing data
+		DELETE	- deleting datas
+	*/
+	
 	function getClassifieds(){
 		return $http.get("data/classifieds.json");
 	}
