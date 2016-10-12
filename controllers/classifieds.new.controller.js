d39c6b21415ee29d00039a31a4b473a6d049a175
Angjel Spasovski <angjel.spasovski@armedia.com>
2016-10-12 17:48:38 +0200

    Setting up Watchers

--- controllers/classifieds.new.controller.js
+++ controllers/classifieds.new.controller.js
@@ -1,10 +1,25 @@
 angular.module("ngClassifieds")
 
-.controller("newClassifiedsCtrl", function($scope, $mdSidenav, $mdDialog, $timeout, classifiedsFactory){
+.controller("newClassifiedsCtrl", function($scope, $mdSidenav, $state, $mdDialog, $timeout, classifiedsFactory){
 	var vm = this;
+
+	vm.closeSidebar = closeSidebar;
+	vm.sidenavOpen;
 
 	$timeout(function(){
 		$mdSidenav('left').open(); 
 	});
+
+	$scope.$watch("vm.sidenavOpen", function(value){
+		if(value === false){
+			$mdSidenav('left').close().then(function(){
+				$state.go('classifieds');
+			});
+		}
+	});
+
+	function closeSidebar(){
+		vm.sidenavOpen = false;
+	}
 	
 })
